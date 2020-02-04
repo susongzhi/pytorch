8937e86a4fabd955dbb1c05839196198cf17dc3b
izdeby <iuriiz@fb.com>
2020-02-04 13:24:12 -0800

    Updated codegen to remove TensorOptions from generated code
    ghstack-source-id: ea3ed7e0f8be282b2ebb13a72fd932067510bc28
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/30983

--- aten/src/ATen/gen_backend_select_register.py
+++ aten/src/ATen/gen_backend_select_register.py
@@ -2,6 +2,7 @@
 # We process only those factory functions that have 'backend_select' flag in its native_functions.yaml definition.
 
 from code_template import CodeTemplate
+import tensor_options_utils as TOUtils
 
 FUNCTION_REGISTRATION = CodeTemplate("""\
 .op(torch::RegisterOperators::options()
@@ -12,7 +13,7 @@ FUNCTION_REGISTRATION = CodeTemplate("""\
 
 FUNCTION_DEFINITION = CodeTemplate("""\
 Tensor ${function_name}(${method_formals}) {
-  DispatchKey key = options.computeDispatchKey();
+  DispatchKey key = TensorOptions::computeDispatchKey(${dispatch_key_args});
   static auto op = c10::Dispatcher::singleton().findSchemaOrThrow("aten::${name}", "${overload_name}");
   return op.callUnboxedWithDispatchKey<${formals_types}>(key, ${type_method_actuals});
 }
@@ -31,18 +32,24 @@ def register_backend_select_methods(declarations, template_path, file_manager):
             if 'overload_name' in option and option['overload_name'] != '':
                 name = "{0}_{1}".format(name, option['overload_name'])
 
-            func_reg = FUNCTION_REGISTRATION.substitute(schema_string=option['schema_string'],
-                                                        function_name=name)
+            if "arguments" in option and TOUtils.check_if_factory_method(option["arguments"]):
+                func_reg = FUNCTION_REGISTRATION.substitute(schema_string=option['schema_string'],
+                                                            function_name=name)
 
-            method_def = FUNCTION_DEFINITION.substitute(function_name=name,
-                                                        method_formals=option['method_formals'],
-                                                        name=option['name'],
-                                                        overload_name=option['overload_name'],
-                                                        formals_types=option['formals_types_with_return'],
-                                                        type_method_actuals=option['type_method_actuals'])
+                if TOUtils.check_hack(name):
+                    dispatch_key_args = "dtype, layout, device"
+                else:
+                    dispatch_key_args = "dtype.value_or(ScalarType::Float), layout.value_or(kStrided), device.value_or(kCPU)"
+                method_def = FUNCTION_DEFINITION.substitute(function_name=name,
+                                                            method_formals=option['method_formals'],
+                                                            name=option['name'],
+                                                            dispatch_key_args=dispatch_key_args,
+                                                            overload_name=option['overload_name'],
+                                                            formals_types=option['formals_types_with_return'],
+                                                            type_method_actuals=option['type_method_actuals'])
 
-            backend_select_function_registrations.append(func_reg)
-            backend_select_method_definitions.append(method_def)
+                backend_select_function_registrations.append(func_reg)
+                backend_select_method_definitions.append(method_def)
 
     env = {}
     env['backend_select_method_definitions'] = backend_select_method_definitions

859c89485ed3faeb27169ac72202d2d9a642637e
Wanchao Liang <wanchaol.home@gmail.com>
2020-02-04 19:17:24 -0500

    [jit] Add RRef to IValue and Jit
    This PR add the Remote Reference type from the torch.distributed.rpc
    package into IValue and JIT. We introduce an RRefInterface abstract
    class and the base of RRef >: UserRRef/OwnerRRef, and made this inherit
    from intrusive_ptr_target, and made the rref to be hold as IValue.
    
    We also add RRefType as a JIT type, it's a container type similar to
    future type.
    
    Note that all of the rpc code in JIT is hidden behind the build flag
    
    ghstack-source-id: bf47c83f6cfc9f597643ca876668818e7781974e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/32992

--- torch/testing/_internal/distributed/rpc/rpc_test.py
+++ torch/testing/_internal/distributed/rpc/rpc_test.py
@@ -1711,3 +1711,24 @@ class RpcTest(RpcAgentTestFixture):
             self.assertTrue(hasattr(this_module, "foo_add"))
             with self.assertRaisesRegex(Exception, "AttributeError"):
                 rpc.rpc_sync(callee_worker, foo_add, args=())
+        self.assertTrue(torch.distributed.rpc.api._default_pickler is _internal_rpc_pickler)
+
+
+@unittest.skipIf(
+    sys.version_info < (3, 0),
+    "Pytorch distributed rpc package " "does not support python2",
+)
+class RpcJitTest(RpcAgentTestFixture):
+    @dist_init
+    def test_rref_as_arg(self):
+        n = self.rank + 1
+        dst_rank = n % self.world_size
+        rref_var = rpc_return_rref("worker{}".format(dst_rank))
+
+        @torch.jit.script
+        def rref_tensor_to_here(rref_var):
+            # type: (RRef[Tensor]) -> Tensor
+            return rref_var.to_here()
+
+        res = rref_tensor_to_here(rref_var)
+        self.assertEqual(res, torch.ones(2, 2) + 1)
